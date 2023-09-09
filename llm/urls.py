2f14c7644dca92b7d29742bb3f02b36507bd1794
jihwan <okpo65@naver.com>
2023-09-09 17:43:57 +0900

    add urls

--- llm/urls.py
+++ llm/urls.py
@@ -6,6 +6,10 @@ import llm.views
 router = routers.SimpleRouter(trailing_slash=False)
 
 urlpatterns = [
-    path('chat/mrc', llm.views.GetMRCResultAPIView.as_view()),
-    path('chat/qa', llm.views.GetQAResultAPIView.as_view()),
+    path('chat/mrc', llm.views.RequestMRCAPIView.as_view()),
+    path('chat/qa', llm.views.RequestQAAPIView.as_view()),
+    path('chat/state', llm.views.PollForResultAPIView.as_view()),
+    path('chat/result', llm.views.GetResultLLMLog.as_view()),
+    path('create/session', llm.views.CreateSessionAPIView.as_view())
+
 ] + router.urls
