72f436cd211de0aee60874347242a8d10a86d4ed
jihwan <okpo65@naver.com>
2023-09-14 16:54:47 +0900

    edit pytest files

--- llm/tests/test.py
+++ llm/tests/test.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from llm.models import LogLLM, LogStateType, LLMTaskType, Session
+import time
 
 @pytest.mark.django_db
 def test_get_mrc_result_api_view_exceeds_token_limit():
@@ -13,17 +15,25 @@ def test_get_mrc_result_api_view_exceeds_token_limit():
     test_data = {
         'question': '이름이 뭐야?',
         'context': '이 프로젝트는  LLM 프로젝트야 ' * 80,
+        'session_id': 2
     }
+    session = Session.objects.create()
+    log = LogLLM.objects.create(question=test_data['question'], session=session, context=test_data['context'],
+                                task_type=LLMTaskType.MRC)
+    log.add_state(state=LogStateType.Pending)
+    from llm.tasks import task_llm_log_processing
+    task_llm_log_processing.apply_async(
+        (log.id,)
+    )
 
-    # Generate the URL for your view
-    url = '/wrtn/chat/mrc'
+    for i in range(10):
+        time.sleep(1)
+        state = log.states.last().state
+        if state != LogStateType.Pending:
+            break
 
-    # Make a POST request to the API view
-    response = client.post(url, data=json.dumps(test_data), content_type='application/json')
+    assert state == LogStateType.Failed
 
-    # Validate the response should be 400 Bad Request
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert 'The total number of tokens in the question and context cannot exceed 512.' in str(response.data)
 
 
 @pytest.mark.django_db
@@ -34,13 +44,24 @@ def test_get_mrc_result_api_view():
     test_data = {
         'question': '일반성면의 면적이 얼마야?',
         'context': '일반성면은 동부 5개 면의 교통, 문화, 교육, 상업의 중심지로서 일찍부터 상업이 발달한 곳으로 날로 반성 재래시장이 번성하고 있는 고장이다. 면적은 19.41 km²로 진주시 16개 읍면동 중 가장 적은 면이지만 인근에 경남 산림환경연구원이 있어 그곳을 찾는 관광객이 이곳 일반성면을 지나간다. 2012년 1월 1일 기준으로 인구 3,233명 (남 : 1,556명, 여 : 1,677명) 1,413세대로 6개 법정리 19개 자연마을 31개 반으로 구성되어 있다.',
+        'session_id': 2
     }
 
     # Generate the URL for your view
-    url = '/wrtn/chat/mrc'
+    session = Session.objects.create()
+    log = LogLLM.objects.create(question=test_data['question'], session=session, context=test_data['context'],
+                                task_type=LLMTaskType.MRC)
+    log.add_state(state=LogStateType.Pending)
+    from llm.tasks import task_llm_log_processing
+    task_llm_log_processing.apply_async(
+        (log.id,)
+    )
 
-    # Make a POST request to the API view
-    response = client.post(url, data=json.dumps(test_data), content_type='application/json')
+    for i in range(10):
+        time.sleep(1)
+        state = log.states.last().state
+        if state != LogStateType.Pending:
+            break
 
-    # Validate the response should be 400 Bad Request
-    assert response.status_code == status.HTTP_200_OK
+
+    assert state == LogStateType.Succeeded
